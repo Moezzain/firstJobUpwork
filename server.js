52a3815c742576468ae8c5f6a1c90b5f9f19cdff
Moez Babiker <moezzain94@gmail.com>
2020-08-15 17:46:37 +0300

    little formating

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-//__Start with my imports__
+// :::::Start with imports::::
+//__Express framework import__
 const express = require('express');
-//__used to ease the access of the .env file__
+//__used to access of the .env file__
 require('dotenv').config();
 //__used to parse the body of the requests to a JSON format for easier handeling of the data__
 const BodyParser = require('body-parser');
@@ -8,19 +9,25 @@ const BodyParser = require('body-parser');
 //__Controlers__
 const APIcontrolers = require('./Controlers/API_Controlers');
 
+// :::::Creat and Start Express App:::::
 const app = express();
+//__listen to specific port that application will be working on__
+//__PORT specified in the .env file__
+app.listen(process.env.PORT || 8080, () =>
+  console.log(`Listening on port ${process.env.PORT || 8080}!`),
+);
 
 //__body parsing handelers__
 app.use(BodyParser.json()); // support json encoded bodies
 app.use(BodyParser.urlencoded({extended: true})); // support url_encoded bodies
 
 
-//          :::::Routes::::
+// :::::::::Routes::::::::
 //__Test Running__
 app.get('/Test', (req, res) => {
     let ret = {};
     ret.Succ = "True";
-      res.send(ret);
+    res.send(ret);
 });
 
 //__Login check credentilas route__
@@ -28,10 +35,3 @@ app.post('/login', APIcontrolers._checkLoginCredentials_);
 
 //__Refresh Service list__
 app.get('/RefreshServicesList', APIcontrolers._Refresh_ServicesList);
-
-
-//__listen to specific port that application will be working on__
-//__PORT specified in the .env file__
-app.listen(process.env.PORT || 8080, () =>
-  console.log(`Listening on port ${process.env.PORT || 8080}!`),
-);
