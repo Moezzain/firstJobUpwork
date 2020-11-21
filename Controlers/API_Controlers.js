79564857845603dbe9bd29a09b2bfa82c63af71d
Moezzain <moezzain94@gmail.com>
2020-11-21 15:41:38 +0200

    absolute path9

--- Controlers/API_Controlers.js
+++ Controlers/API_Controlers.js
@@ -807,7 +807,21 @@ const issuePhpCommand = (scriptId, fileName, callback) => {
   console.log('Running Script...');
   console.log( 'php ' + path.join(__dirname, '..', PhpSaveLocation) + '/' + scripts[scriptId]);
   exec(
-    'php ' + path.join(__dirname, '..', PhpSaveLocation) + '/' + scripts[scriptId]
+    'php ' + path.join(__dirname, '..', PhpSaveLocation) + '/' + scripts[scriptId],
+    (error, stdout, stderr) => {
+      flag++;
+      console.log('flag++ = ' + flag);
+      if (error) {
+        errFlag--;
+        console.log(`error: ${scripts[scriptId]}`);
+        return;
+      }
+      if (stderr) {
+        console.log(`stderr: ${scripts[scriptId]}`);
+        return;
+      }
+      console.log(`stdout: `);
+    },
   );
 };
 
